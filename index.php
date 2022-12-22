e703d124a17792f11fbefd93332c8406437a24a6
Arham Mirza <102862412+arham10jul@users.noreply.github.com>
2022-12-22 18:49:17 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+$age=65;
+if($age>=60)
+{
+    echo "old card";
+
+}
+else if(age>=18)
+{
+    echo "valid for nic";
+
+}
+else
+{
+  echo  "not for valid";
+}
+
+// $marks=-39;
+// if($marks<=100 && $marks>0)
+// {
+//     if($marks=40)
+//     {
+//     echo "pass";
+//     }
+
+
+// else{
+//     echo "fail";
+// }
+// }
+// else
+// {
+//     echo "not valid for marks";
+// }
+
+?>
